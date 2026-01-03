0e041e2e2afd7130ffe1a3b742e4f07389dc64b5
kztkn <februarykazuton02@gmail.com>
2026-01-03 15:39:31 +0900

    basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
 
-  basename: process.env.NODE_ENV === "production" ? "/quiz-app/" : "/",
+  basename: process.env.NODE_ENV === "production" ? "/qz/" : "/",
 } satisfies Config;
